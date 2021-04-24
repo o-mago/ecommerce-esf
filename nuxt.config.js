12aaedaf2bf066177667ea4c0af5c9e80894b1bb
Alexandre Cabral <alexandrecabral@magopc.local>
2021-04-24 18:24:58 -0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/ecommerce-esf/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {

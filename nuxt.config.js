0facf9c73fcd84e44464397965860d635c28502d
Alexandre Cabral <alexandrecabral@magopc.local>
2021-04-26 01:11:25 -0300

    change header title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ecommerce-esf',
+    title: 'Doe ESF-JF',
     htmlAttrs: {
       lang: 'en'
     },

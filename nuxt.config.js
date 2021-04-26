6167c19f275cf0709c9c4c61cdcd5e8d22e87b78
Alexandre Cabral <alexandrecabral@magopc.local>
2021-04-26 00:55:15 -0300

    router test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/ecommerce-esf/'
+  },
   generate: {
     fallback: 'index.html'
   },
